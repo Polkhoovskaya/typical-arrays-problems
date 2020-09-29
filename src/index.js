5a4bf7a0888d4733b833c8a9e8c343c6bd0aed1d
Palkhouskaya <vpalkhouskaya@scnsoft.com>
2020-09-29 15:49:51 +0300

    feat: the task is done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  if (!array || array.length <= 0) return 0;
+
+  let min = (arr) => {
+    return Math.min.apply(Math, arr);
+  }
+
+  return min(array);
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  if (!array || array.length <= 0) return 0;
+
+  let max = (arr) => {
+    return Math.max.apply(Math, arr);
+  }
+
+  return max(array);
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  if (!array || array.length <= 0) return 0;
+
+  let avg = (arr) => {
+    return arr.reduce((a, b) => a + b, 0) / arr.length;
+  }
+
+  return avg(array);
 }
